e82ae2fd570ca11080dda1cbebef76042f0a49ad
LukastheKing <Game.creacion15@gmail.com>
2023-07-22 14:57:01 -0600

    started a test script to understand the logic

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print ("Hello world!")
+# imports
+from pprint import pprint
+
+# Created class for the emails
+class email:
+    def __init__(self, email_address, email_subject, email_content):
+        self.email_address = email_address
+        self.email_subject = email_subject
+        self.email_content = email_content
+
+# Created received email list
+emails = []
+
+#Adding emails to list
+emails.append(email("tomgo@gmail.com","About that game","Hey that game that you told me about..."))
+emails.append(email("JakeWork@gmail.com","You broke the database again","You did it again, the whole app is down.."))
+
+# Printing out the contents
+
+for obj in emails:
+    print("")
+    # Call the vars() on Object
+    obj_vars = vars(obj)
+
+    # Print the properties
+    pprint(obj_vars)
+    
+    print("")
